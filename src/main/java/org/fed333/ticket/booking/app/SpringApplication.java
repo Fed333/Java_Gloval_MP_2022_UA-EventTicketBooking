71dbc5ea9bca635e3c84281b4a711cc2571d317c
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-09-04 13:23:07 +0300

    Manual test of container's work of some BookingFacade methods.

--- src/main/java/org/fed333/ticket/booking/app/SpringApplication.java
+++ src/main/java/org/fed333/ticket/booking/app/SpringApplication.java
@@ -3,15 +3,23 @@ package org.fed333.ticket.booking.app;
 import lombok.extern.slf4j.Slf4j;
 import org.fed333.ticket.booking.app.facade.BookingFacade;
 import org.fed333.ticket.booking.app.facade.impl.BookingFacadeImpl;
+import org.fed333.ticket.booking.app.model.Event;
+import org.fed333.ticket.booking.app.model.Ticket;
 import org.fed333.ticket.booking.app.model.User;
+import org.fed333.ticket.booking.app.model.impl.EventImpl;
+import org.fed333.ticket.booking.app.model.impl.TicketImpl;
 import org.fed333.ticket.booking.app.model.impl.UserImpl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Slf4j
 public class SpringApplication {
 
-    public static void main(String[] args) {
-        //TODO Spring XML-based container raising.
+    public static void main(String[] args) throws ParseException {
+
         log.info("Start raising ApplicationContext...");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:services.xml");
         log.info("ApplicationContext has been raised.");
@@ -22,5 +30,18 @@ public class SpringApplication {
         bookingFacade.createUser(user);
         User userById = bookingFacade.getUserById(user.getId());
         log.info("userById.equals(user) = {}", userById.equals(user));
+
+        Event event = EventImpl.builder()
+                .date(new SimpleDateFormat("dd.MM.yyyy").parse("05.09.2022"))
+                .title("EPAM Townhall").build();
+        bookingFacade.createEvent(event);
+
+        bookingFacade.bookTicket(userById.getId(), event.getId(), 3, Ticket.Category.PREMIUM);
+
+        List<Ticket> bookedUserTickets = bookingFacade.getBookedTickets(user, 10, 1);
+        List<Ticket> bookedEventTickets = bookingFacade.getBookedTickets(event, 10, 1);
+
+        bookedUserTickets.forEach(t->log.info("Booked ticket of user {} - {}", user, t));
+        bookedEventTickets.forEach(t->log.info("Booked ticket of event {} - {}", event, t));
     }
 }
--- src/main/java/org/fed333/ticket/booking/app/model/impl/EventImpl.java
+++ src/main/java/org/fed333/ticket/booking/app/model/impl/EventImpl.java
@@ -1,11 +1,17 @@
 package org.fed333.ticket.booking.app.model.impl;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.fed333.ticket.booking.app.model.Event;
 
 import java.util.Date;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventImpl implements Event {
 
     private Long id;
